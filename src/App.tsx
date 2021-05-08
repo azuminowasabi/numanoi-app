d061434340dfdae990ec4ce5dc49877085a93ed5
あずみのわさび <justtheperson0506@yahoo.co.jp>
2021-05-08 23:19:00 +0900

    huskyの設定を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App:FC = () => (
+const App: FC = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -3,15 +3,15 @@ import { ReportHandler } from 'web-vitals';
 // eslint-disable-next-line
 const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({
-      getCLS, getFID, getFCP, getLCP, getTTFB,
-    }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    }).catch((err) => console.error(err)); // eslint-disable-line no-console
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      })
+      .catch((err) => console.error(err)); // eslint-disable-line no-console
   }
 };
 
