c828be0380c3cbebbedbb6300f2a175c2abd06a7
あずみのわさび <justtheperson0506@yahoo.co.jp>
2021-05-11 23:57:51 +0900

    家の外と家の中の分類分けをデータに追加

--- src/data/places.ts
+++ src/data/places.ts
@@ -2,17 +2,62 @@ export type Place = {
   code: string;
   name: string;
   desc: string;
+  details: string[];
 };
 
 export const places: Place[] = [
   {
     code: 'Inside',
     name: '屋内',
-    desc: 'キッチン、トイレ、和室、ダイニング...',
+    desc: '家の中',
+    details: [
+      'entrance',
+      'kitchen',
+      'bath',
+      'restroom',
+      'dining',
+      'jproom1',
+      'jproom2',
+      'annex',
+    ],
   },
   {
     code: 'Outside',
     name: '屋外',
     desc: '東側庭、南側庭、西側庭、駐車場...',
+    details: [
+      'steet',
+      'parking',
+      'pond',
+      'front-entrance',
+      'east-garden',
+      'south-garden',
+      'west-garden',
+      'shoukyakuro1',
+    ],
   },
 ];
+
+/**
+ * 家の中
+ *  玄関
+ *  キッチン
+ *  風呂
+ *  トイレ
+ *  ダイニング
+ *  和室1
+ *  和室2
+ *  天井
+ *  離れ
+ *
+ * 家の外
+ *  道
+ *  駐車場
+ *  池
+ *  入り口前
+ *  東庭 (倉庫の場所)
+ *  南庭 (第二焼却炉)
+ *  西庭 (杭のある場所)
+ *  第一焼却炉
+ *
+ */
